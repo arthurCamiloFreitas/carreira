9339c3fb936b4e470d4b95bc3a394e29e8c65be4
arthurCamiloFreitas <arthur.freitas19@etec.sp.gov.br>
2023-08-29 13:55:53 -0300

    sistema de templates

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 require_once __DIR__ . '/Router.php';
 $request = $_SERVER['REQUEST_URI'];
 
-//var_dump($request);
-//die();
 
+$template = new Template();
+$template->header();
 (new Router())->run($request);
+$template->footer();
